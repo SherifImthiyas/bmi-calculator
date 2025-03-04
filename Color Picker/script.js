d4e3d5df5dd63bcc213b39ee0891cc8143c12a06
SherifImthiyas <ind.shadowcreations@gmail.com>
2025-03-04 09:33:58 +0530

    Add files via upload

---
+++ Color Picker/script.js
@@ -0,0 +1,10 @@
+const bg = document.getElementById('Cont');
+const Hex = document.getElementById('hex');
+const code = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
+
+
+function hexcode() {
+    let cre = [code.Math.floor(Math.random() * 5)]
+    console.log(cre)
+}
+
